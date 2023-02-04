71ccdfa95c8593fd80d4b6394cad5e5ee4f39e09
Leoni <118344314+lktay@users.noreply.github.com>
2023-02-04 11:04:12 +0000

    add token to local storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   const CLIENT_ID = "4081228661664398aa13c58607ba9675";
   const REDIRECT_URI = "http://localhost:3000";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
   return (
     <div>
+      <h1>Spotify API</h1>
       <a
         href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
       >
